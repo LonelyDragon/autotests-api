d62f8980ad02b158c85ff808f5b570bf2de58eb6
Zavodchikov Anton <azavodchikov@ocs.ru>
2025-12-08 22:38:19 +0300

    Практикуемся в использовании API клиентов. Реализуем private builder для HTTP-клиента

--- clients/courses/courses_client.py
+++ clients/courses/courses_client.py
@@ -5,19 +5,16 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
 
 
 class GetCoursesQueryDict(TypedDict):
-    """
-    Описание структуры запроса на получение списка курсов.
-    """
+    """Описание структуры запроса на получение списка курсов."""
     userId: str
 
 
 class CreateCourseRequestDict(TypedDict):
-    """
-    Описание структуры запроса на создание курса.
-    """
+    """Описание структуры запроса на создание курса."""
     title: str
     maxScore: int
     minScore: int
@@ -39,13 +36,10 @@ class UpdateCourseRequestDict(TypedDict):
 
 
 class CoursesClient(APIClient):
-    """
-    Клиент для работы с /api/v1/courses
-    """
+    """Клиент для работы с /api/v1/courses."""
 
     def get_courses_api(self, query: GetCoursesQueryDict) -> Response:
-        """
-        Метод получения списка курсов.
+        """Метод получения списка курсов.
 
         :param query: Словарь с userId.
         :return: Ответ от сервера в виде объекта httpx.Response
@@ -53,8 +47,7 @@ class CoursesClient(APIClient):
         return self.get("/api/v1/courses", params=query)
 
     def get_course_api(self, course_id: str) -> Response:
-        """
-        Метод получения курса.
+        """Метод получения курса.
 
         :param course_id: Идентификатор курса.
         :return: Ответ от сервера в виде объекта httpx.Response
@@ -62,8 +55,7 @@ class CoursesClient(APIClient):
         return self.get(f"/api/v1/courses/{course_id}")
 
     def create_course_api(self, request: CreateCourseRequestDict) -> Response:
-        """
-        Метод создания курса.
+        """Метод создания курса.
 
         :param request: Словарь с title, maxScore, minScore, description, estimatedTime,
         previewFileId, createdByUserId.
@@ -72,8 +64,7 @@ class CoursesClient(APIClient):
         return self.post("/api/v1/courses", json=request)
 
     def update_course_api(self, course_id: str, request: UpdateCourseRequestDict) -> Response:
-        """
-        Метод обновления курса.
+        """Метод обновления курса.
 
         :param course_id: Идентификатор курса.
         :param request: Словарь с title, maxScore, minScore, description, estimatedTime.
@@ -82,10 +73,17 @@ class CoursesClient(APIClient):
         return self.patch(f"/api/v1/courses/{course_id}", json=request)
 
     def delete_course_api(self, course_id: str) -> Response:
-        """
-        Метод удаления курса.
+        """Метод удаления курса.
 
         :param course_id: Идентификатор курса.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/courses/{course_id}")
+
+
+def get_courses_client(user: AuthenticationUserDict) -> CoursesClient:
+    """Функция создаёт экземпляр CoursesClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию CoursesClient.
+    """
+    return CoursesClient(client=get_private_http_client(user))
--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -3,25 +3,21 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
 
 
 class CreateFileRequestDict(TypedDict):
-    """
-    Описание структуры запроса на создание файла.
-    """
+    """Описание структуры запроса на создание файла."""
     filename: str
     directory: str
     upload_file: str
 
 
 class FilesClient(APIClient):
-    """
-    Клиент для работы с /api/v1/files
-    """
+    """Клиент для работы с /api/v1/files."""
 
     def get_file_api(self, file_id: str) -> Response:
-        """
-        Метод получения файла.
+        """Метод получения файла.
 
         :param file_id: Идентификатор файла.
         :return: Ответ от сервера в виде объекта httpx.Response
@@ -29,8 +25,7 @@ class FilesClient(APIClient):
         return self.get(f"/api/v1/files/{file_id}")
 
     def create_file_api(self, request: CreateFileRequestDict) -> Response:
-        """
-        Метод создания файла.
+        """Метод создания файла.
 
         :param request: Словарь с filename, directory, upload_file.
         :return: Ответ от сервера в виде объекта httpx.Response
@@ -42,10 +37,17 @@ class FilesClient(APIClient):
         )
 
     def delete_file_api(self, file_id: str) -> Response:
-        """
-        Метод удаления файла.
+        """Метод удаления файла.
 
         :param file_id: Идентификатор файла.
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/files/{file_id}")
+
+
+def get_files_client(user: AuthenticationUserDict) -> FilesClient:
+    """Функция создаёт экземпляр FilesClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию FilesClient.
+    """
+    return FilesClient(client=get_private_http_client(user))
