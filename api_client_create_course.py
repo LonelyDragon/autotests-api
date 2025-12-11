0026af23c28f5e6642e6c85e68cb026b54c372de
Zavodchikov Anton <azavodchikov@ocs.ru>
2025-12-11 22:47:28 +0300

    Применяем Pydantic в API клиентах. Применяем Pydantic в AuthenticationClient

--- api_client_create_course.py
+++ api_client_create_course.py
@@ -1,6 +1,6 @@
 from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
 from clients.files.files_client import get_files_client, CreateFileRequestDict
-from clients.private_http_builder import AuthenticationUserDict
+from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
 from tools.fakers import get_random_email
 
@@ -8,14 +8,14 @@ public_users_client = get_public_users_client()
 
 create_user_request = CreateUserRequestDict(
     email=get_random_email(),
-    password="string",
-    lastName="string",
+    password="string123",
+    lastName="string12",
     firstName="string",
     middleName="string"
 )
 create_user_response = public_users_client.create_user(create_user_request)
 
-authentication_user = AuthenticationUserDict(
+authentication_user = AuthenticationUserSchema(
     email=create_user_request['email'],
     password=create_user_request['password']
 )
