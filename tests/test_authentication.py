592fc4d338cb3012e9ad9262c3f81e4fd4fc0ae0
Zavodchikov Anton <azavodchikov@ocs.ru>
2026-01-06 00:36:15 +0300

    Фикстуры Pytest. Переносим создание пользователя в фикстуру

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -4,8 +4,7 @@ import pytest
 
 from clients.authentication.authentication_client import AuthenticationClient
 from clients.authentication.authentication_schema import LoginResponseSchema, LoginRequestSchema
-from clients.users.public_users_client import PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
@@ -13,11 +12,12 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login(public_users_client: PublicUsersClient, authentication_client: AuthenticationClient):
-    create_user_request = CreateUserRequestSchema()
-    public_users_client.create_user(create_user_request)
+def test_login(
+        function_user: UserFixture,  # Используем фикстуру для создания пользователя
+        authentication_client: AuthenticationClient
+):
     login_response = authentication_client.login_api(
-        LoginRequestSchema(email=create_user_request.email, password=create_user_request.password)
+        LoginRequestSchema(email=function_user.email, password=function_user.password)
     )
     assert_status_code(login_response.status_code, HTTPStatus.OK)
     login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
